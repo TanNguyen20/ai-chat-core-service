c8a1b7c02165721b70194b2a3f66efb582cac81d
Tan Nguyen <82394019+TanNguyen20@users.noreply.github.com>
2025-09-18 14:14:55 +0700

    update: use Superset MCP remote server

--- configs/server.py
+++ configs/server.py
@@ -1,29 +1,7 @@
 server_config = {
     "mcpServers": {
         "superset": {
-            # e.g. "C:\\Users\\NhatTan\\.local\\bin\\uv.EXE"
-            "command": "C:Users/NhatTan/.local/bin/uv.EXE",
-            "args": [
-                "run",
-                "--with", "fastapi",
-                "--with", "httpx",
-                "--with", "mcp[cli]",
-                "--with", "python-dotenv",
-                "--with", "uvicorn",
-                "mcp", "run",
-                "F:/ai-core-service/mcp-server/superset-mcp/main.py",
-            ],
-        },
-        "VN-Tech-Classifier": {
-            "command": "uv",
-            "args": [
-                "run",
-                "--with",
-                "fastmcp",
-                "fastmcp",
-                "run",
-                "..\\mcp\\classification\\main.py"
-            ]
+            "url": "https://superset-mcp.vercel.app/mcp",
         }
     }
 }
